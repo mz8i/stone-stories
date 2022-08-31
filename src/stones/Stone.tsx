4e2364ba9d58023694df79efc2c8f83c6eebd297
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-08-31 13:17:19 +0200

    WIP switch to rapier physics

--- src/stones/Stone.tsx
+++ src/stones/Stone.tsx
@@ -3,58 +3,60 @@ import { useItemStore } from '../item-store';
 import { Grab } from '../lib/react-xr-default-hands/Grab';
 import { StoneModel } from './StoneModel';
 import { useStoneUpload } from './stone-upload';
-import { STONE_MASS, useStonePhysics } from './use-stone-physics';
+import { STONE_MASS } from './use-stone-physics';
+import { MeshCollider, RigidBody } from '@react-three/rapier';
+import { RayGrab } from '@react-three/xr';
 
 export function Stone({ itemId, ...props }) {
   const set = useItemStore((store) => store.set);
   const item = useItemStore((store) => store.items[itemId]);
 
-  const [ref, api] = useStonePhysics(itemId);
+  // const [ref, api] = useStonePhysics(itemId);
   const [isGrabbing, setIsGrabbing] = useState(false);
 
-  useEffect(() => {
-    if (!item.frozen) {
-      if (isGrabbing) {
-        api.mass.set(0);
-        api.allowSleep.set(false);
-        api.wakeUp();
+  // useEffect(() => {
+  //   if (!item.frozen) {
+  //     if (isGrabbing) {
+  //       api.mass.set(0);
+  //       api.allowSleep.set(false);
+  //       api.wakeUp();
 
-        if (item.levitating) {
-          set((store) => {
-            store.items[itemId].touched = true;
-          });
-        }
-      } else {
-        if (item.levitating && item.touched) {
-          api.mass.set(STONE_MASS);
-          api.allowSleep.set(false);
-          api.wakeUp();
-          set((store) => {
-            store.items[itemId].levitating = false;
-          });
-        }
-        if (!item.levitating) {
-          api.mass.set(STONE_MASS);
-        }
-        api.allowSleep.set(true);
-      }
-    }
-  }, [isGrabbing, api, item.frozen, item.levitating, set, itemId, item.touched, item]);
+  //       if (item.levitating) {
+  //         set((store) => {
+  //           store.items[itemId].touched = true;
+  //         });
+  //       }
+  //     } else {
+  //       if (item.levitating && item.touched) {
+  //         api.mass.set(STONE_MASS);
+  //         api.allowSleep.set(false);
+  //         api.wakeUp();
+  //         set((store) => {
+  //           store.items[itemId].levitating = false;
+  //         });
+  //       }
+  //       if (!item.levitating) {
+  //         api.mass.set(STONE_MASS);
+  //       }
+  //       api.allowSleep.set(true);
+  //     }
+  //   }
+  // }, [isGrabbing, api, item.frozen, item.levitating, set, itemId, item.touched, item]);
 
-  useStoneUpload(item, api, set);
+  // useStoneUpload(item, api, set);
 
-  const model = <StoneModel ref={ref} type={item.model} virtual={!item.touched && item.id.startsWith('_')} />;
+  const model = <StoneModel type={item.model} virtual={!item.touched && item.id.startsWith('_')} />;
 
-  return !item.frozen ? (
-    <Grab
-      physicsApi={api}
-      onChange={({ isGrabbed }) => {
-        setIsGrabbing(isGrabbed);
-      }}
+  const rigidBody = (
+    <RigidBody
+      type={item.frozen || item.levitating ? 'fixed' : isGrabbing ? 'kinematicPosition' : 'dynamic'}
+      colliders="hull"
+      position={item.position}
+      rotation={item.rotation}
     >
       {model}
-    </Grab>
-  ) : (
-    model
+    </RigidBody>
   );
+
+  return item.frozen ? rigidBody : <RayGrab>{rigidBody}</RayGrab>;
 }
--- src/stones/use-stone-physics.ts
+++ src/stones/use-stone-physics.ts
@@ -5,56 +5,56 @@ import { stoneMetadata } from './StoneModel';
 
 export const STONE_MASS = 1;
 
-export function useStonePhysics(itemId) {
-  const set = useItemStore((store) => store.set);
-  const item = useItemStore((store) => store.items[itemId]);
+// export function useStonePhysics(itemId) {
+//   const set = useItemStore((store) => store.set);
+//   const item = useItemStore((store) => store.items[itemId]);
 
-  const [ref, api] = useBox(
-    () => {
-      const options: any = {
-        args: stoneMetadata[item.model].physicsBox,
-        position: item.position,
-        rotation: item.rotation,
-      };
+//   const [ref, api] = useBox(
+//     () => {
+//       const options: any = {
+//         args: stoneMetadata[item.model].physicsBox,
+//         position: item.position,
+//         rotation: item.rotation,
+//       };
 
-      if (!item.frozen && !item.levitating) {
-        options.mass = STONE_MASS;
-        options.sleepSpeedLimit = 0.5;
-      }
+//       if (!item.frozen && !item.levitating) {
+//         options.mass = STONE_MASS;
+//         options.sleepSpeedLimit = 0.5;
+//       }
 
-      return options;
-    },
-    null,
-    [item.frozen, item.levitating],
-  );
+//       return options;
+//     },
+//     null,
+//     [item.frozen, item.levitating],
+//   );
 
-  useEffect(() => {
-    if (!api) return;
+//   useEffect(() => {
+//     if (!api) return;
 
-    set((store) => {
-      store.items[itemId].api = api;
-    });
-  }, [api, itemId, set]);
+//     set((store) => {
+//       store.items[itemId].api = api;
+//     });
+//   }, [api, itemId, set]);
 
-  useEffect(() => {
-    return api?.position.subscribe((position) => {
-      set((store) => {
-        if (store.items[itemId]) {
-          store.items[itemId].position = position;
-        }
-      });
-    });
-  }, [api, itemId, set]);
+//   useEffect(() => {
+//     return api?.position.subscribe((position) => {
+//       set((store) => {
+//         if (store.items[itemId]) {
+//           store.items[itemId].position = position;
+//         }
+//       });
+//     });
+//   }, [api, itemId, set]);
 
-  useEffect(() => {
-    return api?.rotation.subscribe((r) => {
-      set((store) => {
-        if (store.items[itemId]) {
-          store.items[itemId].rotation = r;
-        }
-      });
-    });
-  }, [api, itemId, set]);
+//   useEffect(() => {
+//     return api?.rotation.subscribe((r) => {
+//       set((store) => {
+//         if (store.items[itemId]) {
+//           store.items[itemId].rotation = r;
+//         }
+//       });
+//     });
+//   }, [api, itemId, set]);
 
-  return [ref, api] as const;
-}
+//   return [ref, api] as const;
+// }
